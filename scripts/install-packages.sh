2ce2051d348ce484c0a9ba2714995b5f709e2443
Pratham Patel <thefirst1322@gmail.com>
2023-03-08 07:54:11 +0000

    fix typo for pkg archlinux-keyring

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -4,7 +4,7 @@
 # packages to be installed
 ################################################################################
 
-PKGS_TO_INSTALL=(parchlinux-keyring base bash bind btrfs-progs cronie curl dash dosfstools e2fsprogs exfatprogs findutils gcc git git-lfs htop iotop iperf iperf3 iputils less linux-firmware lm_sensors lsb-release lsof man man-db man-pages mlocate nano networkmanager opendoas openssh openssl pacman-contrib sudo tar tmux vim wget xfsprogs xz zip zsh zstd)
+PKGS_TO_INSTALL=(archlinux-keyring base bash bind btrfs-progs cronie curl dash dosfstools e2fsprogs exfatprogs findutils gcc git git-lfs htop iotop iperf iperf3 iputils less linux-firmware lm_sensors lsb-release lsof man man-db man-pages mlocate nano networkmanager opendoas openssh openssl pacman-contrib sudo tar tmux vim wget xfsprogs xz zip zsh zstd)
 
 # extra pkgs
 #PKGS_TO_INSTALL+=(aria2 base-devel bat btop choose dog dua-cli dust exa fd hyperfine namcap neovim nload procs ripgrep rsync rustup skim tealdeer tre tree unrar unzip wget yt-dlp zsh-autosuggestions zsh-completions zsh-syntax-highlighting)
