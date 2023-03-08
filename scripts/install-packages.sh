fde2ab2a518f2e43c0231bb8894e08534e36c645
Pratham Patel <thefirst1322@gmail.com>
2023-03-08 07:50:29 +0000

    move a few packages from a minimal system (that are installed) inside the optional packages list

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -4,11 +4,10 @@
 # packages to be installed
 ################################################################################
 
-PKGS_TO_INSTALL=(archlinux-keyring base base-devel bash bind btrfs-progs cron curl dash dosfstools e2fsprogs exfatprogs findutils gcc git git-lfs htop inxi iotop iperf iperf3 iputils less linux-firmware lm_sensors lsb-release lsof man man-db man-pages mlocate namcap nano neovim networkmanager opendoas openssh openssl pacman-contrib rsync sudo tar tmux unrar unzip vim wget xfsprogs xz zip zsh zstd)
-
+PKGS_TO_INSTALL=(parchlinux-keyring base bash bind btrfs-progs cronie curl dash dosfstools e2fsprogs exfatprogs findutils gcc git git-lfs htop iotop iperf iperf3 iputils less linux-firmware lm_sensors lsb-release lsof man man-db man-pages mlocate nano networkmanager opendoas openssh openssl pacman-contrib sudo tar tmux vim wget xfsprogs xz zip zsh zstd)
 
 # extra pkgs
-#PKGS_TO_INSTALL+=(aria2 bat btop choose dog dua-cli dust exa fd hyperfine nload procs ripgrep rustup skim tealdeer tre tree wget yt-dlp zsh-autosuggestions zsh-completions zsh-syntax-highlighting)
+#PKGS_TO_INSTALL+=(aria2 base-devel bat btop choose dog dua-cli dust exa fd hyperfine namcap neovim nload procs ripgrep rsync rustup skim tealdeer tre tree unrar unzip wget yt-dlp zsh-autosuggestions zsh-completions zsh-syntax-highlighting)
 
 
 ################################################################################
