e23987125dd318ef686876e0ad22683895a50b3d
Pratham Patel <thefirst1322@gmail.com>
2023-03-11 13:34:16 +0530

    pkg zsh should not be in the base, move it to the optional pkgs section

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -4,10 +4,10 @@
 # packages to be installed
 ################################################################################
 
-PKGS_TO_INSTALL=(archlinux-keyring base bash bind btrfs-progs cronie curl dash dosfstools e2fsprogs exfatprogs findutils gcc git git-lfs htop iotop iperf iputils less linux-firmware lm_sensors lsb-release lsof man man-db man-pages mlocate nano networkmanager opendoas openssh openssl pacman-contrib strace sudo tar tmux vim wget which xfsprogs xz zip zsh zstd)
+PKGS_TO_INSTALL=(archlinux-keyring base bash bind btrfs-progs cronie curl dash dosfstools e2fsprogs exfatprogs findutils gcc git git-lfs htop iotop iperf iputils less linux-firmware lm_sensors lsb-release lsof man man-db man-pages mlocate nano networkmanager opendoas openssh openssl pacman-contrib strace sudo tar tmux vim wget which xfsprogs xz zip zstd)
 
 # extra pkgs
-#PKGS_TO_INSTALL+=(aria2 base-devel bat btop choose dog dua-cli dust exa fd hyperfine iperf3 namcap neovim nload procs ripgrep rsync rustup skim tealdeer tre tree unrar unzip wget yt-dlp zsh-autosuggestions zsh-completions zsh-syntax-highlighting)
+#PKGS_TO_INSTALL+=(aria2 base-devel bat btop choose dog dua-cli dust exa fd hyperfine iperf3 namcap neovim nload procs ripgrep rsync rustup skim tealdeer tre tree unrar unzip wget yt-dlp zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting)
 
 
 ################################################################################
