f843754db00b22df784a515218ed859da04d7ad6
Ankur Kotwal <ankur.kotwal@gmail.com>
2023-03-12 00:38:58 +1100

    Expand package list as per suggestions from @thefossguy. Fix package install script

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -4,7 +4,7 @@
 # install packages
 ################################################################################
 
-PKGS_TO_INSTALL="${CONF_PKGS_TO_INSTALL}"
+PKGS_TO_INSTALL=(`echo "${CONF_PKGS_TO_INSTALL}"`)
 pacstrap -C extra/pacman.conf /mnt "${PKGS_TO_INSTALL[@]}"
 
 # vim:set ts=4 sts=4 sw=4 et:
